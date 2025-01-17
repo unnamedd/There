f05b0b0607032921029e52631421d4f56ef7e51a
Thiago Holanda <unnamedd@gmail.com>
2025-01-17 04:11:29 +0100

    Add support to download profile images from Bluesky

--- There/Views/Timezone/IconSection.swift
+++ There/Views/Timezone/IconSection.swift
@@ -59,8 +59,22 @@ struct SocialMediaInput: View {
 
                             if !Task.isCancelled {
                                 do {
-                                    let imageUrl = "https://unavatar.io/\(platform.lowercased())/\(value.lowercased())"
-                                    let fetchedImage = try await simpleImageFetch(from: imageUrl)
+                                    let username = value.lowercased()
+
+                                    // FIXME: While Unavatar does not support Bluesky, we've decided
+                                    // to extract the user profile picture directly from the Bluesky API.
+                                    // However current implementation should be rolled back after Unavatar
+                                    // receives proper support for it.
+                                    let imageURL = switch source {
+                                    case .bluesky:
+                                        try await fetchBlueskyProfileImageURL(for: username)
+                                        
+                                    default:
+                                        "https://unavatar.io/\(source.id)/\(username)"
+                                    }
+
+                                    let fetchedImage = try await simpleImageFetch(from: imageURL)
+
                                     await MainActor.run {
                                         self.image = NSImage(data: fetchedImage)
                                     }
@@ -83,11 +97,35 @@ struct SocialMediaInput: View {
 
         let (data, response) = try await URLSession.shared.data(from: url)
 
-        guard let httpResponse = response as? HTTPURLResponse else {
+        guard let _ = response as? HTTPURLResponse else {
             throw URLError(.badServerResponse)
         }
 
         image = NSImage(data: data)
         return data
     }
+
+    // This method should not be exposed to callers outside of this file
+    private func fetchBlueskyProfileImageURL(for username: String) async throws -> String {
+        let urlString = "https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=\(username)"
+
+        guard let url = URL(string: urlString) else {
+            throw URLError(.badURL)
+        }
+
+        let (data, response) = try await URLSession.shared.data(from: url)
+
+        guard let _ = response as? HTTPURLResponse else {
+            throw URLError(.badServerResponse)
+        }
+
+        let decoder = JSONDecoder()
+        let bluesky = try decoder.decode(Bluesky.self, from: data)
+
+        return bluesky.avatar
+    }
+}
+
+private struct Bluesky: Decodable {
+    let avatar: String
 }
